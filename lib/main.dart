9c3f35f10d690eb8888d09a12ea23e488e1fa4c6
下宮 一真 <shimomiya_k@mti.co.jp>
2019-05-14 18:39:06 +0900

    add chip and checkbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:setting_card_sample/model/filter.dart';
 
 void main() => runApp(MyApp());
 
@@ -66,46 +67,281 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        appBar: AppBar(
+          elevation: 0.0,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        backgroundColor: Colors.blue,
+        body: Column(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            SizedBox(
+              height: 40.0,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                itemCount: 2,
+                itemBuilder: (context, index) {
+                  if (index == 0)
+                    return Container(
+                      child: _Card(
+                        child: _CheckBoxView(
+                          title: 'CheckBox Filter',
+                        ),
+                      ),
+                    );
+
+                  return Container(
+                    child: _Card(
+                        child: _ChipView(
+                      title: 'Chip Filter',
+                    )),
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 40.0,
             ),
           ],
+        ));
+  }
+}
+
+const _kDefaultCardRadius = 40.0;
+
+class _Card extends StatelessWidget {
+  final Widget child;
+
+  const _Card({
+    Key key,
+    @required this.child,
+  })  : assert(child != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(_kDefaultCardRadius),
+          bottomLeft: Radius.circular(_kDefaultCardRadius),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      elevation: 16.0,
+      child: child,
+    );
+  }
+}
+
+class _CheckBoxView extends StatefulWidget {
+  final List<int> currentValues;
+
+  final String title;
+
+  const _CheckBoxView({
+    Key key,
+    this.currentValues = const [],
+    this.title,
+  }) : super(key: key);
+
+  @override
+  __CheckBoxViewState createState() => __CheckBoxViewState();
+}
+
+class __CheckBoxViewState extends State<_CheckBoxView> {
+  List<Filter> filters = [
+    Filter(0, 'Filter 0'),
+    Filter(1, 'Filter 1'),
+    Filter(2, 'Filter 2'),
+    Filter(3, 'Filter 3'),
+    Filter(4, 'Filter 4'),
+    Filter(5, 'Filter 5'),
+  ];
+
+  List<int> currentValues;
+
+  @override
+  void initState() {
+    currentValues = widget.currentValues;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding:
+          EdgeInsets.only(left: _kDefaultCardRadius + 8.0, top: 16.0, bottom: 16.0, right: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(widget.title),
+          Container(
+            height: 1.0,
+            width: double.infinity,
+            color: Colors.grey[400],
+          ),
+          Column(
+            children: List.generate(filters.length, (index) {
+              return _CheckBox(
+                value: filters[index].value,
+                currentValues: currentValues,
+                title: Text(filters[index].text),
+                onChanged: (values) {
+                  setState(() {
+                    currentValues = values;
+                  });
+                },
+              );
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CheckBox extends StatelessWidget {
+  final int value;
+
+  final List<int> currentValues;
+
+  final Widget title;
+
+  final Function(List<int> values) onChanged;
+
+  const _CheckBox({
+    Key key,
+    @required this.value,
+    @required this.currentValues,
+    @required this.title,
+    this.onChanged,
+  })  : assert(value != null),
+        assert(currentValues != null),
+        assert(title != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CheckboxListTile(
+      value: currentValues.contains(value),
+      onChanged: (bool value) {
+        List<int> result = List.from(currentValues);
+        if (currentValues.contains(this.value))
+          result.remove(this.value);
+        else
+          result.add(this.value);
+
+        onChanged(result);
+      },
+      title: title,
+    );
+  }
+}
+
+class _ChipView extends StatefulWidget {
+  final List<int> currentValues;
+
+  final String title;
+
+  const _ChipView({
+    Key key,
+    this.currentValues = const [],
+    this.title,
+  }) : super(key: key);
+
+  @override
+  __ChipViewState createState() => __ChipViewState();
+}
+
+class __ChipViewState extends State<_ChipView> {
+  List<Filter> filters = [
+    Filter(0, 'Filter 0'),
+    Filter(1, 'Filter 1'),
+    Filter(2, 'Filter 2'),
+    Filter(3, 'Filter 3'),
+    Filter(4, 'Filter 4'),
+    Filter(5, 'Filter 5'),
+  ];
+
+  List<int> currentValues;
+
+  @override
+  void initState() {
+    currentValues = widget.currentValues;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding:
+          EdgeInsets.only(left: _kDefaultCardRadius + 8.0, top: 16.0, bottom: 16.0, right: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(widget.title),
+          Container(
+            height: 1.0,
+            width: double.infinity,
+            color: Colors.grey[400],
+          ),
+          Wrap(
+            children: List.generate(filters.length, (index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: _Chip(
+                  value: filters[index].value,
+                  currentValues: currentValues,
+                  text: filters[index].text,
+                  onChanged: (values) {
+                    setState(() {
+                      currentValues = values;
+                    });
+                  },
+                ),
+              );
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Chip extends StatelessWidget {
+  final int value;
+
+  final List<int> currentValues;
+
+  final String text;
+
+  final Function(List<int> values) onChanged;
+
+  const _Chip({
+    Key key,
+    this.text,
+    this.value,
+    this.currentValues,
+    this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChoiceChip(
+      label: Text(text),
+      selected: currentValues.contains(value),
+      onSelected: (value) {
+        List<int> result = List.from(currentValues);
+        if (currentValues.contains(this.value))
+          result.remove(this.value);
+        else
+          result.add(this.value);
+
+        onChanged(result);
+      },
     );
   }
 }
---
+++ lib/model/filter.dart
@@ -0,0 +1,9 @@
+class Filter {
+  final int value;
+  final String text;
+
+  Filter(
+    this.value,
+    this.text,
+  );
+}
